19887be54f70521f78442fd102411d3d168eb970
lostkangaroo <lostkangaroo@lostkangaroo.net>
2014-05-09 19:29:09 -0500

    add dlamp Apache 2 License

--- cookbooks/dlamp/recipes/default.rb
+++ cookbooks/dlamp/recipes/default.rb
@@ -1,3 +1,23 @@
+#
+# Author:: Andrew Jungklaus <lostkangaroo@lostkangaroo.net>
+# Cookbook Name:: dlamp
+# Recipe:: default
+# Description:: Sets a runlist and default values to use in building a Drupal 8
+#   Development Environment
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Default Values to use
 
 # MySQL
