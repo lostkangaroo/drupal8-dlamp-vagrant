1bc567cc6cce5653f13123887496e97338ea7ca3
lostkangaroo <lostkangaroo@lostkangaroo.net>
2015-08-21 18:15:48 -0500

    add mysql2 gem cookbook

---
+++ cookbooks/mysql2_chef_gem/Berksfile
@@ -0,0 +1,9 @@
+source 'https://supermarket.chef.io'
+
+metadata
+
+group :integration do
+  cookbook 'apt'
+end
+
+cookbook 'mysql2_chef_gem_test', path: 'test/fixtures/cookbooks/mysql2_chef_gem_test'
---
+++ cookbooks/mysql2_chef_gem/metadata.rb
@@ -0,0 +1,19 @@
+name 'mysql2_chef_gem'
+maintainer 'Nicolas Blanc'
+maintainer_email 'sinfomicien@gmail.com'
+license 'Apache 2.0'
+description 'Provides the mysql2_chef_gem resource'
+
+version '1.0.2'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
+
+depends 'build-essential'
+depends 'mysql', '~> 6.0'
+depends 'mariadb'
