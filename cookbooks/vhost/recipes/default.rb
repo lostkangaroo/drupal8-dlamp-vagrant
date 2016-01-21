3e771af976a3bcdec9aef4bd3ccf860e9b1d9e28
Andrew Jungklaus <ajungklaus@apqc.org>
2016-01-21 16:01:45 -0600

    allow vhost to use databag defined hosts

--- cookbooks/vhost/recipes/default.rb
+++ cookbooks/vhost/recipes/default.rb
@@ -1,5 +1,18 @@
 include_recipe "apache2"
 
+Chef::Log.info "Attempting to locate databag vhosts"
+if Chef::DataBag.list.key?('vhost')
+  begin
+    vhosts = data_bag('vhost').collect do |item|
+      vhost = data_bag_item('vhost', item)
+
+      node['vhost'] << vhost
+    end
+  rescue
+    Chef::Log.info "Could not load data bag 'vhost'"
+  end
+end
+
 node['vhost'].each do |host|
   web_app "#{host['localhost_alias']}" do
     server_name "#{host['localhost_alias']}"
